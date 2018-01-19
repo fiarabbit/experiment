2c027a9aa3828306597242ff4bbe7b433ecd8893
Yuki Hashimoto <hashimoto@hal.t.u-tokyo.ac.jp>
2018-01-19 13:29:32 +0900

    debug

--- src/Model/MySQL.php
+++ src/Model/MySQL.php
@@ -201,6 +201,7 @@ class MySQL {
             throw new \Exception('NoUserSQL');
         }
         $query = $this->assocToINSERT($srvAssoc, 'AnotherTransaction');
+        var_dump($query);exit();
         if ($this->mysqli->query($query)) {
             return true;
         } else {
