3bb60d8127c546f9429986c3678b459a5b02c644
Yuki Hashimoto <hashimoto@hal.t.u-tokyo.ac.jp>
2018-01-24 15:00:02 +0900

    video stream

--- web/assets/js/login.js
+++ web/assets/js/login.js
@@ -1,12 +1,11 @@
 document.getElementById("jscheck").textContent = "enabled";
 if (document.cookie.search(/cookie=true/) !== -1) {
     document.getElementById("cookiecheck").textContent = "enabled";
-    document.getElementById("input").disabled = "";
 }
 
 navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
-        getUserMedia: function(c) {
-            return new Promise(function(y, n) {
+        getUserMedia: function (c) {
+            return new Promise(function (y, n) {
                 (navigator.mozGetUserMedia ||
                 navigator.webkitGetUserMedia).call(navigator, c, y, n);
             });
@@ -19,17 +18,25 @@ if (!navigator.mediaDevices) {
 
 // Prefer camera resolution nearest to 1280x720.
 
-var constraints = { audio: true, video: { width: {ideal: 1280}, height: {ideal: 720} } };
+var constraints = {audio: true, video: {width: {ideal: 1280}, height: {ideal: 720}}};
 
 navigator.mediaDevices.getUserMedia(constraints)
-    .then(function(stream) {
+    .then(function (stream) {
         var video = document.getElementById('video');
         video.src = window.URL.createObjectURL(stream);
-        video.onloadedmetadata = function(e) {
+        video.onloadedmetadata = function (e) {
             document.getElementById('videocheck').textContent = 'enabled';
             video.play();
         };
     })
-    .catch(function(err) {
+    .catch(function (err) {
         console.log(err.name + ": " + err.message);
     });
+
+var repeat = setInterval(
+    function () {
+        if (document.getElementById("jscheck") === "enabled" && document.getElementById("cookiecheck") === "enabled" && document.getElementById("videocheck") === "enabled") {
+            document.getElementById("input").disabled = "";
+            clearInterval(repeat);
+        }
+    }, 1000);
